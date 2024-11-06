6484153bfbe591e850d2043d3c664a39960c44fe
Ramsfield <ramsfield@ramsfield.org>
2024-11-05 18:57:31 -0800

    Adds initial docstrings

--- plexsel.py
+++ plexsel.py
@@ -34,17 +34,19 @@ class PlexSel:
 		self.close()
 
 	# Wrapper functions to make working with Selenium easier
-
 	def buttonExists(self, css_desc):
+		# Returns true if a button exists at this point in time
 		return len(self._driver.find_elements(By.CSS_SELECTOR, css_desc)) > 0
 	
 	def buttonPress(self, css_desc):
+		# Presses a button if it exists
 		if not self.buttonExists(css_desc):
 			return False
 		self._driver.find_elements(By.CSS_SELECTOR, css_desc)[0].click()
 		return True
 	
 	def timeoutPress(self, css_desc, timeout=15):
+		# Waits for a button to exist and then presses it
 		start = time.time()
 		now = start
 		pressed = self.buttonPress(css_desc)
@@ -54,51 +56,69 @@ class PlexSel:
 		return pressed
 
 	def pressHome(self, timeout=15):
+		# Press the 'Home' button
+		# TODO: Add logic to make this callable from anywhere
 		return self.timeoutPress(self._button_lookup["home"], timeout)
 	
 	def start(self):
+		# Starts up the selenium driver and navigates to the home page
 		self._driver.get(self._url)
 		while not self.pressHome(0):
 			self.timeoutPress(self._button_lookup["firstExperience"], 1)
 	
 	def playMovie(self, timeout=15):
+		# Presses the "Play Movie" button to start playback
 		return self.timeoutPress(self._button_lookup["startPlay"], timeout)
 
 	def getMovieByIndex(self, index):
+		# Navigates to a movie's url. This uses the movie index from the plex server api
+		# this api will have no knowledge of the plex server stuff, it expects you to know
+		# what movie index you feed it
+		# TODO: Make sure this works from any page
 		self._driver.get(f"{self._url}{self._movie_base}{index}")
 
 	def currentlyActive(self):
+		# Determines if a video is active
 		return self.buttonExists(self._button_lookup["activePlayPause"])
 
 	def currentlyPlaying(self):
+		# FIXME: Figure out wether or not the video is actually playing
 		while not self.buttonExists(self._button_lookup["activePlayPause"]):
 			time.sleep(1)
 		help(self._driver.find_elems(By.CSS_SELECTOR, self._button_lookup["activePlayPause"])[0])
 
 	def toggleFullscreen(self):
-		#Will toggle the fullscreen status of the movie -- Will need to figure out how to track fullscreen status
+		# Will toggle the fullscreen status of the movie
+		# TODO: need to figure out how to track fullscreen status
 		return self.timeoutPress(self._button_lookup["activeFullscreen"])
 	
 	def isMini(self):
+		# Determines if there is a video active and we're in Mini Mode
 		return self.currentlyActive() and self.buttonExists(self._button_lookup["activeToMaxi"])
 
 	def toMini(self):
+		# Sets to mini mode if we're in maxi mode
 		if not self.isMaxi():
 			return False
 		return self.timeoutPress(self._button_lookup["activeToMini"], 1)
 
 	def isMaxi(self):
+		# Determines if we're in Maxi Mode
 		return self.currentlyActive() and self.buttonExists(self._button_lookup["activeToMini"])
 
 	def toMaxi(self):
+		# Sets to max mode
 		if not self.isMini():
 			return False
 		return timeoutPress(self._button_lookup["activeToMaxi"], 1)
 
 	def toggleMaxi(self):
+		# Toggles player between mini and maxi
 		if self.isMaxi():
 			return self.toMini()
-		return self.toMaxi()
+		elif self.isMini():
+			return self.toMaxi()
+		return False
 
 	def close(self):
 		self._driver.close()
